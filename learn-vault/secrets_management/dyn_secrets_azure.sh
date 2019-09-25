373801a2ebd11142266204f96ecfde90c76c368b
Bas <bas.meijer@me.com>
2019-09-25 23:44:32 +0200

    Vault scripts updated (#6)

--- learn-vault/secrets_management/dyn_secrets_azure.sh
+++ learn-vault/secrets_management/dyn_secrets_azure.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 vault secrets list|grep azure || vault secrets enable azure
 
--- learn-vault/secrets_management/dyn_secrets_dev.sh
+++ learn-vault/secrets_management/dyn_secrets_dev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Enable the versioned k/v secrets engine like the dev server has it.
-vault secrets list|grep secret || vault secrets enable -path=secret/ -version=2 kv
+vault secrets list|grep secret\/ || vault secrets enable -path=secret/ -version=2 kv
