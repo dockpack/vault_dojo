2df3d00483c1b72ceabf95be1546f3fedaf400e4
Bas Meijer <bas.meijer@me.com>
2019-09-25 23:14:12 +0200

    secret

--- learn-vault/policies/create_policies.sh
+++ learn-vault/policies/create_policies.sh
@@ -5,7 +5,7 @@ vault policy write admin admin-policy.hcl
 vault policy write policies policies-policy.hcl
 
 # Create secret policy
-vault policy write policies secret-policy.hcl
+vault policy write secret secret-policy.hcl
 
 # Create engines policy
 vault policy write engines engines-policy.hcl
