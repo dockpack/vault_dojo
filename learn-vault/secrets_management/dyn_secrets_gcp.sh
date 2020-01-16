e59e0adfb4943484773afa8d2775a16f0d3f6a5b
Bas Meijer <bas.meijer@me.com>
2020-01-16 23:05:31 +0100

    GCP

--- learn-vault/secrets_management/dyn_secrets_gcp.sh
+++ learn-vault/secrets_management/dyn_secrets_gcp.sh
@@ -4,4 +4,14 @@ vault secrets list|grep gcp || vault secrets enable gcp
 
 vault write gcp/config credentials="@$GOOGLE_APPLICATION_CREDENTIALS"
 
+vault write gcp/roleset/my-token-roleset \
+project=$GCP_PROJECT \
+secret_type="access_token"  \
+token_scopes="https://www.googleapis.com/auth/cloud-platform" \
+bindings=-<<EOF
+  resource "//cloudresourcemanager.googleapis.com/projects/$GCP_PROJECT_ID" {
+    roles = ["roles/viewer"]
+  }
+EOF
+
 vault path-help gcp
