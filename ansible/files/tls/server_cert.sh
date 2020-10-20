cfde9c67635993a27af4934362bd9ba19295de6b
Bas Meijer <bas.meijer@me.com>
2020-10-20 16:50:26 +0200

    Fixes SAN certs

--- ansible/files/tls/server_cert.sh
+++ ansible/files/tls/server_cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-consul tls cert create -server
+consul tls cert create -server -additional-dnsname=server01.consul -additional-dnsname=server02.consul -additional-dnsname=server03.consul -days 512
